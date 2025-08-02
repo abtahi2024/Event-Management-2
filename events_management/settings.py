9df666f0b51acfc2885e382a30b092719c7b8cb3
Abtahi chowdhury <abtahejisan@gmail.com>
2025-08-02 10:12:04 +0600

    now ready

--- events_management/settings.py
+++ events_management/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com','http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
 
 
 
